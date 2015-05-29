4a811aaa21555d05e846c4e437572fcc57c8cace
scnzhg <zhongjian.gong@sas.com>
2015-05-29 11:06:45 +0800

    add demo controller

---
+++ src/springmvc/controller/DemoController.java
@@ -0,0 +1,23 @@
+package springmvc.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import springmvc.model.DemoModel;
+
+/**
+ * Created by scnzhg on 5/29/2015.
+ */
+public class DemoController {
+
+    @RequestMapping(value = "/model/{modelId}", method = RequestMethod.GET)
+    public @ResponseBody DemoModel getModels(@PathVariable("modelId") String modelId){
+        System.out.println("enter the method");
+        DemoModel model =new DemoModel();
+        model.setName("123");
+        model.setDescription("desc");
+        return model;
+    }
+}
