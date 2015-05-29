17515fcefa2dbd5b7146c8af259b6e994d996ca9
scnzhg <zhongjian.gong@sas.com>
2015-05-29 11:40:09 +0800

    first springmvc configuration

--- src/springmvc/controller/DemoController.java
+++ src/springmvc/controller/DemoController.java
@@ -1,5 +1,6 @@
 package springmvc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +11,8 @@ import springmvc.model.DemoModel;
 /**
  * Created by scnzhg on 5/29/2015.
  */
+@Controller
 public class DemoController {
-
     @RequestMapping(value = "/model/{modelId}", method = RequestMethod.GET)
     public @ResponseBody DemoModel getModels(@PathVariable("modelId") String modelId){
         System.out.println("enter the method");
